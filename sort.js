b8bc86ae30091f04c7f98e9271b456d50365bf7c
tskon <tsekhmeister.k@gmail.com>
2019-05-12 18:48:34 +0300

    merge sorted

--- sort.js
+++ sort.js
@@ -1,4 +1,4 @@
-const getRandomNumber = () => Math.round(Math.random() * 100)
+const getRandomNumber = () => Math.round(Math.random() * 100000)
 const getArray = (n) => {
   const array = [0]
   for (let i = 0; i < n; i++) {
@@ -51,3 +51,52 @@ const insertSort = (arr) => {
 timer(() => insertSort(array))
 
 // merge sort algorithm
+const mergeSort = (arr2) => {
+  const arr = [...arr2]
+  const merge = (a, b) => {
+    let ai = 0
+    let bi = 0
+    const result = []
+
+    for (let i = 0; i < a.length + b.length; i++) {
+      if (a[ai] < b[bi] || b[bi] === undefined) {
+        result.push(a[ai])
+        ai++
+      } else {
+        result.push(b[bi])
+        bi++
+      }
+    }
+
+    return result
+  }
+
+  const newArrays = []
+
+  // init sorted arrays
+  for (let i = 0; i < arr.length; i += 2) {
+    if (arr[i + 1]) {
+      newArrays.push(merge([arr[i]], [arr[i + 1]]))
+    } else {
+      newArrays.push([arr[i]])
+    }
+  }
+
+  // merge sorted arrays
+  const mergeSortedArrays = (sortedArraysList) => {
+    const result = []
+    for (let i = 0; i < sortedArraysList.length; i += 2) {
+      if (sortedArraysList[i + 1]) {
+        result.push(merge(sortedArraysList[i], sortedArraysList[i + 1]))
+      } else {
+        result.push(sortedArraysList[i])
+      }
+    }
+    if (result.length > 1) return mergeSortedArrays(result)
+    return result[0]
+  }
+
+  console.log('mergeSort', mergeSortedArrays(newArrays))
+}
+
+timer(() => mergeSort(array))
