f259273ec1b9ad680d141c3c8e0609790c242baf
tskon <tsekhmeister.k@gmail.com>
2019-05-12 17:41:51 +0300

    insert sort

--- sort.js
+++ sort.js
@@ -6,7 +6,7 @@ const getArray = (n) => {
   }
   return array
 }
-const array = getArray(5)
+const array = getArray(10000)
 
 const timer = (func) => {
   const startTime = +new Date()
@@ -19,9 +19,9 @@ const timer = (func) => {
 const bubbleSort = (arr) => {
   const newArr = [...arr]
 
-  for(let i = 0; i < newArr.length; i++ ){
-    for(let j = 0; j < newArr.length - i; j++) {
-      if (newArr[j] > newArr[j+1]) [newArr[j+1], newArr[j]] = [newArr[j], newArr[j+1]]
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = 0; j < newArr.length - i; j++) {
+      if (newArr[j] > newArr[j + 1]) [newArr[j + 1], newArr[j]] = [newArr[j], newArr[j + 1]]
     }
   }
   console.log('bubbleSort:', newArr)
@@ -30,7 +30,21 @@ const bubbleSort = (arr) => {
 timer(() => bubbleSort(array))
 
 // insert sort algorithm
+const insertSort = (arr) => {
+  const newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i])
+    for (let j = newArr.length - 1; j >= 0; j--) {
+      if (j > 0 && newArr[j] < newArr[j - 1]) {
+        [newArr[j], newArr[j - 1]] = [newArr[j - 1], newArr[j]]
+      } else {
+        break
+      }
+    }
+  }
+  console.log('insertSort:', newArr)
+}
 
-
+timer(() => insertSort(array))
 
 // merge sort algorithm
