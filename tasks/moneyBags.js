00308f77284adc5ca03b5dbaa6f39246aa0bf456
tskon <tsekhmeister.k@gmail.com>
2019-05-22 23:21:55 +0300

    init moneyBags

--- tasks/moneyBags.js
+++ tasks/moneyBags.js
@@ -3,6 +3,9 @@ checkConfiguration(2, [2,6], 5)
 function checkConfiguration(bagsCount, listOfSum, total){
   if (listOfSum.length !== bagsCount || Math.max(...listOfSum) > total ) return console.log('No')
 
+  // Todo добавить поиск возможных комбинаций
+  // Todo добавить проверку на то влезают ли при наличии успешной комбинации один кошелек в другой
+
   if (listOfSum.reduce((sum, num) => sum + num, 0) === total){
     return console.log('Yes')
   }
